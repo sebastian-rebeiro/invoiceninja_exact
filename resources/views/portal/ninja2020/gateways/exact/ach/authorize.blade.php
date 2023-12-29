e0d4c31a4cb35fdcb453cf0b752c97ef499c0225
Sebastian Rebeiro <sebastian@rebeiro.ca>
2023-12-29 06:26:32 +0000

    chore: Adding Terms to ACH

--- resources/views/portal/ninja2020/gateways/exact/ach/authorize.blade.php
+++ resources/views/portal/ninja2020/gateways/exact/ach/authorize.blade.php
@@ -1,5 +1,34 @@
 @extends('portal.ninja2020.layout.payments', ['gateway_title' => 'Bank Details', 'card_title' => 'Bank Details'])
 
+@section('gateway_head')
+<style>
+    #collapsible {
+        background-color: #eee;
+        color: #444;
+        cursor: pointer;
+        padding: 18px;
+        width: 100%;
+        border: none;
+        text-align: left;
+        outline: none;
+        font-size: 15px;
+    }
+
+    /* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+    .active, #collapsible:hover {
+        background-color: #ccc; 
+    }
+
+    /* Style the collapsible content. Note: hidden by default */
+    #collapsible-content {
+        padding: 0 18px;
+        display: none;
+        overflow: hidden;
+        background-color: #f1f1f1;
+    }
+</style>
+@endsection
+
 @section('gateway_content')
     @if(session()->has('ach_error'))
         <div class="alert alert-failure mb-4">
@@ -43,7 +72,23 @@
         @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.account_number')])
             <input class="input w-full" id="account-number" type="text" required>
         @endcomponent
-
+        <button id="collapsible" type="button" onclick="var content = document.getElementById('collapsible-content');if (content.style.display === 'block') {content.style.display = 'none';} else {content.style.display = 'block';}">Terms</button>
+        <div id="collapsible-content">
+            @component('portal.ninja2020.components.general.card-element-single', ['title' => 'SEPA', 'show_title' => false])
+            <p>By adding this to your payment methods, you accept this Agreement and authorize {{ $company->present()->name() }} to debit the specified bank account for any amount owed for charges arising from the use of services and/or purchase of products.</p>
+            <br>
+            <p>Payments will be debited from the specified account when an invoice becomes due.</p>
+            <br>
+            <p>Where a scheduled debit date is not a business day, {{ $company->present()->name() }} will debit on the next business day.</p>
+            <br>
+            <p>You agree that any payments due will be debited from your account immediately upon acceptance of this Agreement and that confirmation of this Agreement may be sent within 5 (five) days of acceptance of this Agreement. You further agree to be notified of upcoming debits up to 1 (one) day before payments are collected.</p>
+            <br>
+            <p>You have certain recourse rights if any debit does not comply with this agreement. For example, you have the right to receive reimbursement for any debit that is not authorized or is not consistent with this PAD Agreement. To obtain more information on your recourse rights, contact your financial institution.</p>
+            <br>
+            <p>You may amend or cancel this authorization at any time by providing the merchant with thirty (30) days notice at {{ $company->present()->email() }}. To obtain a sample cancellation form, or further information on cancelling a PAD agreement, please contact your financial institution.</p>
+            <br>
+            @endcomponent
+        </div>
         @component('portal.ninja2020.components.general.card-element-single')
             <input type="checkbox" class="form-checkbox mr-1" name="accept_terms" id="accept-terms" required>
             <label for="accept-terms" class="cursor-pointer">{{ ctrans('texts.ach_authorization', ['company' => auth()->user()->company->present()->name, 'email' => auth('contact')->user()->client->company->settings->email]) }}</label>
